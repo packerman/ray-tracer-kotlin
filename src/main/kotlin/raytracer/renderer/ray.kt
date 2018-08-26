038c6ab783f298fe219f3042345d2556d470cb76
Piotr Ackermann <pioacker@gmail.com>
2018-08-26 23:39:44 +0200

    Phong lighting

--- src/main/kotlin/raytracer/renderer/ray.kt
+++ src/main/kotlin/raytracer/renderer/ray.kt
@@ -1,31 +1,9 @@
 package raytracer.renderer
 
-import raytracer.math.*
-import kotlin.math.sqrt
-
-class Sphere {
-    var transform: Matrix4 = Matrix4.IDENTITY
-
-    fun intersect(ray: Ray): List<Intersection> {
-        val ray2 = ray.transform(this.transform.inverse())
-
-        val sphereToRay = ray2.origin - point(0f, 0f, 0f)
-
-        val a = ray2.direction dot ray2.direction
-        val b = (ray2.direction dot sphereToRay) * 2f
-        val c = (sphereToRay dot sphereToRay) - 1f
-
-        val discriminant = b * b - 4f * a * c
-        if (discriminant < 0f) {
-            return intersections()
-        }
-
-        val i1 = Intersection((-b - sqrt(discriminant)) / (2f * a), this)
-        val i2 = Intersection((-b + sqrt(discriminant)) / (2f * a), this)
-
-        return if (i1.t > i2.t) intersections(i2, i1) else intersections(i1, i2)
-    }
-}
+import raytracer.math.Matrix4
+import raytracer.math.Point
+import raytracer.math.Vector
+import raytracer.math.times
 
 data class Ray(val origin: Point, val direction: Vector) {
     fun position(t: Float): Point = origin + direction * t
---
+++ src/test/kotlin/raytracer/renderer/MaterialTest.kt
@@ -0,0 +1,20 @@
+package raytracer.renderer
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import raytracer.math.color
+
+internal class MaterialTest {
+
+    @Test
+    fun defaultMaterial() {
+        val m = Material()
+
+        assertEquals(color(1f, 1f, 1f), m.color)
+        assertEquals(0.1f, m.ambient)
+        assertEquals(0.9f, m.diffuse)
+        assertEquals(0.9f, m.specular)
+        assertEquals(200f, m.shininess)
+    }
+
+}
