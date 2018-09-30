fe26f9c8f1e2eabeb39e4c2718ad2de54ea8776b
Piotr Ackermann <pioacker@gmail.com>
2018-09-30 23:47:20 +0200

    Add objects to the scene

--- src/main/kotlin/raytracer/loader/load.kt
+++ src/main/kotlin/raytracer/loader/load.kt
@@ -1,6 +1,7 @@
 package raytracer.loader
 
 import raytracer.renderer.*
+import java.lang.Math.toRadians
 
 data class Scene(val cameras: List<Camera>, val world: World)
 
@@ -34,9 +35,36 @@ fun loadScene(loaded: Any): Scene {
             camera.transform = viewTransform(from, to, up)
 
             builder.cameras.add(camera)
+        } else if (elem["add"] == "light") {
+            val position = readPoint(elem["at"])
+            val intensity = readColor(elem["intensity"])
+            builder.lights.add(PointLight(position, intensity))
+        } else if (elem["add"] == "plane") {
+            val shape = Plane()
+            setObjectProperties(shape, elem)
+            builder.shapes.add(shape)
+        } else if (elem["add"] == "sphere") {
+            val shape = Sphere()
+            setObjectProperties(shape, elem)
+            builder.shapes.add(shape)
+        } else if (elem["add"] == "cube") {
+            val shape = Cube()
+            setObjectProperties(shape, elem)
+            builder.shapes.add(shape)
         }
     }
     return builder.build()
+}
+
+fun setObjectProperties(shape: Shape, elem: Map<String, *>) {
+    @Suppress("UNCHECKED_CAST") val material = elem["material"] as Map<String, *>
+    shape.material = Material(
+            color = readColor(material["color"]),
+            ambient = readFloat(material["ambient"]),
+            diffuse = readFloat(material["diffuse"]),
+            specular = readFloat(material["specular"])
+    )
+    shape.transform = readTransforms(elem["transform"])
 }
 
 fun readInt(elem: Any?) = (elem as Number).toInt()
@@ -45,15 +73,39 @@ fun readFloat(elem: Any?) = (elem as Number).toFloat()
 
 fun readPoint(elem: Any?): Point {
     val list = elem as List<*>
-    return point(readFloat(elem[0]), readFloat(elem[1]), readFloat(elem[2]))
+    require(list.size == 3)
+    return point(readFloat(list[0]), readFloat(list[1]), readFloat(list[2]))
 }
 
 fun readVector(elem: Any?): Vector {
     val list = elem as List<*>
-    return vector(readFloat(elem[0]), readFloat(elem[1]), readFloat(elem[2]))
+    require(list.size == 3)
+    return vector(readFloat(list[0]), readFloat(list[1]), readFloat(list[2]))
 }
 
 fun readColor(elem: Any?): Vector {
     val list = elem as List<*>
-    return color(readFloat(elem[0]), readFloat(elem[1]), readFloat(elem[2]))
+    require(list.size == 3)
+    return color(readFloat(list[0]), readFloat(list[1]), readFloat(list[2]))
+}
+
+fun readTransform(elem: Any?): Matrix4 {
+    val list = elem as List<*>
+    val name = list[0] as String
+    if (name == "rotate-x") {
+        return rotationX(toRadians(readFloat(list[1]).toDouble()).toFloat())
+    } else if (name == "translate") {
+        return translation(readFloat(list[1]), readFloat(list[2]), readFloat(list[3]))
+    }
+    TODO()
+}
+
+fun readTransforms(elem: Any?): Matrix4 {
+    var transform = Matrix4.identity
+    val transforms = elem as List<*>
+
+    transforms.map(::readTransform).forEach {
+        transform = it * transform
+    }
+    return transform
 }
--- src/test/kotlin/raytracer/loader/LoaderTest.kt
+++ src/test/kotlin/raytracer/loader/LoaderTest.kt
@@ -2,13 +2,13 @@ package raytracer.loader
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.instanceOf
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.snakeyaml.engine.v1.api.Load
 import org.snakeyaml.engine.v1.api.LoadSettingsBuilder
-import raytracer.renderer.point
-import raytracer.renderer.vector
-import raytracer.renderer.viewTransform
+import raytracer.renderer.*
+import java.lang.Math.toRadians
 
 internal class LoaderTest {
 
@@ -46,4 +46,161 @@ internal class LoaderTest {
         assertEquals(expectedTransform, camera.transform)
     }
 
+    @Test
+    fun canAddLight() {
+        val yamlString = """
+            |- add: light
+            |  at: [50, 100, -50]
+            |  intensity: [1, 1, 1]
+            |
+            |- add: light
+            |  at: [-400, 50, -10]
+            |  intensity: [0.2, 0.2, 0.2]
+        """.trimMargin()
+
+        val settings = LoadSettingsBuilder()
+                .build()
+        val load = Load(settings)
+
+        val loaded = load.loadFromString(yamlString)
+
+        val scene = loadScene(loaded)
+
+        assertThat(scene.world.lights, hasSize(2))
+
+        val expectedLight0 = PointLight(
+                position = point(50f, 100f, -50f),
+                intensity = color(1f, 1f, 1f)
+        )
+        val expectedLight1 = PointLight(
+                position = point(-400f, 50f, -10f),
+                intensity = color(0.2f, 0.2f, 0.2f)
+        )
+
+        assertEquals(expectedLight0, scene.world.lights[0])
+        assertEquals(expectedLight1, scene.world.lights[1])
+    }
+
+    @Test
+    fun canAddPlane() {
+        val yamlString = """
+            |- add: plane
+            |  material:
+            |    color: [1, 1, 1]
+            |    ambient: 1
+            |    diffuse: 0
+            |    specular: 0
+            |  transform:
+            |    - [ rotate-x, 90]
+            |    - [ translate, 0, 0, -500]
+            |
+        """.trimMargin()
+
+        val settings = LoadSettingsBuilder()
+                .build()
+        val load = Load(settings)
+        val loaded = load.loadFromString(yamlString)
+        val scene = loadScene(loaded)
+
+        assertThat(scene.world, hasSize(1))
+
+        assertThat(scene.world[0], instanceOf(Plane::class.java))
+
+        val plane = scene.world[0] as Plane
+
+        val expectedMaterial = Material(
+                color = color(1f, 1f, 1f),
+                ambient = 1f,
+                diffuse = 0f,
+                specular = 0f
+        )
+
+        val expectedTransform = rotationX(toRadians(90.0).toFloat())
+                .translate(0f, 0f, -500f)
+
+        assertEquals(expectedMaterial, plane.material)
+        assertEquals(expectedTransform, plane.transform)
+    }
+
+    @Test
+    fun canAddSphere() {
+        val yamlString = """
+            |- add: sphere
+            |  material:
+            |    color: [1, 1, 1]
+            |    ambient: 1
+            |    diffuse: 0
+            |    specular: 0
+            |  transform:
+            |    - [ rotate-x, 90]
+            |    - [ translate, 0, 0, -500]
+            |
+        """.trimMargin()
+
+        val settings = LoadSettingsBuilder()
+                .build()
+        val load = Load(settings)
+        val loaded = load.loadFromString(yamlString)
+        val scene = loadScene(loaded)
+
+        assertThat(scene.world, hasSize(1))
+
+        assertThat(scene.world[0], instanceOf(Sphere::class.java))
+
+        val plane = scene.world[0] as Sphere
+
+        val expectedMaterial = Material(
+                color = color(1f, 1f, 1f),
+                ambient = 1f,
+                diffuse = 0f,
+                specular = 0f
+        )
+
+        val expectedTransform = rotationX(toRadians(90.0).toFloat())
+                .translate(0f, 0f, -500f)
+
+        assertEquals(expectedMaterial, plane.material)
+        assertEquals(expectedTransform, plane.transform)
+    }
+
+    @Test
+    fun canAddCube() {
+        val yamlString = """
+            |- add: cube
+            |  material:
+            |    color: [1, 1, 1]
+            |    ambient: 1
+            |    diffuse: 0
+            |    specular: 0
+            |  transform:
+            |    - [ rotate-x, 90]
+            |    - [ translate, 0, 0, -500]
+            |
+        """.trimMargin()
+
+        val settings = LoadSettingsBuilder()
+                .build()
+        val load = Load(settings)
+        val loaded = load.loadFromString(yamlString)
+        val scene = loadScene(loaded)
+
+        assertThat(scene.world, hasSize(1))
+
+        assertThat(scene.world[0], instanceOf(Cube::class.java))
+
+        val plane = scene.world[0] as Cube
+
+        val expectedMaterial = Material(
+                color = color(1f, 1f, 1f),
+                ambient = 1f,
+                diffuse = 0f,
+                specular = 0f
+        )
+
+        val expectedTransform = rotationX(toRadians(90.0).toFloat())
+                .translate(0f, 0f, -500f)
+
+        assertEquals(expectedMaterial, plane.material)
+        assertEquals(expectedTransform, plane.transform)
+    }
 }
